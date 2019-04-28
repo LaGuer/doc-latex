1ce1ce858c96dc229e2b27781cb6af19968038d5
Laurent G. OSSBSS AMIS UCC <27735554+LaGuer@users.noreply.github.com>
2019-04-28 09:02:41 -0700

    Update build-main.sh

--- .ci/build-main.sh
+++ .ci/build-main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-if git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'paper/'
+if git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'publication/'
 then
   # Install tectonic using conda
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
@@ -22,7 +22,7 @@ then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan $TRAVIS_BRANCH-pdf
   git rm -rf .
-  git add -f paper/ms.pdf
+  git add -f publication/main.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
   git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
